824f5a650576efc56e284c14b3dcb32ede897508
btea <2356281422@qq.com>
2021-10-03 16:03:35 +0800

    chore: 添加element-plus

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import ElementPlus from 'unplugin-element-plus/vite';
 
 export default defineConfig({
 	server: {
 		host: true
 	},
-	plugins: [vue()]
+	plugins: [vue(), ElementPlus()]
 	// base: './'
 });
